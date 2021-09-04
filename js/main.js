30057ae496001bd855da5ad3fcabc695afeb3e69
anggakurnia2911 <anggakurnia2911@gmail.com>
2021-09-05 02:35:11 +0700

    perbaikan Responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,78 @@
-AOS.init();
-function validateForm() {
-    var x = document.forms["form-regis"]["email"].value;
-    var atpos = x.indexOf("@");
-    var dotpos = x.lastIndexOf(".");
-    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
-        alert("Not a valid e-mail address");
+document.querySelector('#email').addEventListener('blur', validateEmail);
+
+document.querySelector('#password').addEventListener('blur', validatePassword);
+
+document.querySelector('#username').addEventListener('blur', validateUsername);
+
+const reSpaces = /^\S*$/;
+
+function validateUsername(e) {
+    const username = document.querySelector('#username');
+    if (reSpaces.test(username.value)) {
+        username.classList.remove('is-invalid');
+        username.classList.add('is-valid');
+        return true;
+    } else {
+        username.classList.remove('is-valid');
+
+        username.classList.add('is-invalid');
         return false;
     }
 }
+
+function validateEmail(e) {
+    const email = document.querySelector('#email');
+    const re = /^([a-zA-Z0-9_\-?\.?]){3,}@([a-zA-Z]){3,}\.([a-zA-Z]){2,5}$/;
+
+    if (reSpaces.test(email.value) && re.test(email.value)) {
+        email.classList.remove('is-invalid');
+        email.classList.add('is-valid');
+
+        return true;
+    } else {
+        email.classList.add('is-invalid');
+        email.classList.remove('is-valid');
+
+        return false;
+    }
+}
+
+function validatePassword() {
+    const password = document.querySelector('#password');
+    const re = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})(?=.*[!@#$%^&*])/;
+    if (re.test(password.value) && reSpaces.test(password.value)) {
+        password.classList.remove('is-invalid');
+        password.classList.add('is-valid');
+
+        return true;
+    } else {
+        password.classList.add('is-invalid');
+        password.classList.remove('is-valid');
+
+        return false;
+    }
+}
+
+(function () {
+    const forms = document.querySelectorAll('.needs-validation');
+
+    for (let form of forms) {
+        form.addEventListener(
+            'submit',
+            function (event) {
+                if (
+                    !form.checkValidity() ||
+                    !validateEmail() ||
+                    !validateUsername() ||
+                    !validatePassword()
+                ) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                } else {
+                    form.classList.add('was-validated');
+                }
+            },
+            false
+        );
+    }
+})();
